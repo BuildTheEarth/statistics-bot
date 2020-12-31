9fe8d9558fc9974aaed3a0ee493d2e93efc4eec5
cAttte <26514199+cAttte@users.noreply.github.com>
2020-12-31 04:41:41 -0300

    feat: create Command class

---
+++ src/struct/Command.ts
@@ -0,0 +1,24 @@
+import Discord from "discord.js"
+
+export default abstract class Command {
+    static command: string
+    static description: string
+    static aliases: string[]
+    static options: CommandOption[]
+    static dev: boolean = false
+
+    static subcommands: Command[] = []
+    static run: (
+        message: Discord.Message,
+        options: { [name: string]: unknown }
+    ) => Promise<void>
+}
+
+export interface CommandOption<R = boolean, P = boolean> {
+    positional: P
+    name: string
+    abbr?: P extends true ? null : string
+    required?: R extends true ? true : false
+    default?: R extends true ? null : unknown
+    description: string
+}
