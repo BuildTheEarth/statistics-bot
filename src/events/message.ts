5dc6b32800b717ec98e1e3263f2f393effe1b799
cAttte <26514199+cAttte@users.noreply.github.com>
2021-02-06 14:23:16 -0300

    feat(events): create basic 'message' handler

---
+++ src/events/message.ts
@@ -0,0 +1,21 @@
+import Discord from "discord.js"
+import Client from "../struct/Client"
+
+export default async function message(
+    this: Client,
+    message: Discord.Message
+): Promise<unknown> {
+    if (message.author.bot) return
+    if (!message.content.startsWith(this.config.prefix)) return
+
+    const body = message.content.slice(this.config.prefix.length).trim()
+    const split = body.split(" ")
+    const name = split[0].toLowerCase()
+    const options = split.slice(1).join(" ").trim()
+    options // unused variable placeholder
+
+    const command = this.commands.find(c => c.name === name || c.aliases.includes(name))
+    if (!command) return
+
+    await command.run(message, {})
+}
